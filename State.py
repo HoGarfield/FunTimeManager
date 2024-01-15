c4a6f16f10001fbf02b2bd3a2a891ed85bc26192
garfield_ho <garfield_ho@qq.com>
2024-01-15 17:58:46 +0800

    重构使用状态机框架

---
+++ State.py
@@ -0,0 +1,185 @@
+import datetime
+import math
+from threading import Timer
+
+
+class StateOwner:
+
+	def __init__(self, config):
+		self.config = config
+		self.state = IdleState(self, config)
+
+	def try_to(self, state_cls, sender, msg):
+		if state_cls.can_switch_to(self.config, sender, msg):
+			old_state = self.state
+			old_state.on_exit(msg.user)
+
+			self.state = state_cls(self, self.config)
+			self.state.on_enter(msg.user)
+
+	def text_reply(self, msg):
+		if msg.user.NickName == "个个都是靓仔靓女":
+			sender = msg.user.search_member(userName=msg.ActualUserName)
+			print(msg.text, sender)
+
+			self.state.text_reply(sender, msg)
+
+
+class StateBase:
+
+	def __init__(self, owner, config):
+		self.owner = owner
+		self.config = config
+
+	def on_enter(self, user):
+		pass
+
+	def on_exit(self, user):
+		pass
+
+	@classmethod
+	def can_switch_to(cls, config, sender, msg):
+		return True
+
+	def text_reply(self, sender, msg):
+		if msg.text == "指令列表":
+			cmd = """
+			开始娱乐
+			停止娱乐
+			"""
+			for study_type in self.config["study"]:
+				if type(self.config["study"][study_type]) == list:
+					cmd += f"""
+					开始{study_type}
+					{study_type}赢了
+					{study_type}输了
+					"""
+				else:
+					cmd += f"""
+					开始{study_type}
+					停止{study_type}
+					"""
+
+			msg.user.send_msg(cmd)
+
+
+class IdleState(StateBase):
+
+	def text_reply(self, sender, msg):
+		super().text_reply(sender, msg)
+
+		if sender.NickName == "cmdr" and msg.text == "开始娱乐":
+			self.owner.try_to(FunState, sender, msg)
+		elif sender.NickName == "cmdr" and msg.text.startswith("开始"):
+			self.owner.try_to(StudyState, sender, msg)
+
+
+class FunState(StateBase):
+	RestTime = 0
+	StartFunTime = None
+	WarningT = None
+
+	def on_enter(self, user):
+		super().on_enter(user)
+
+		FunState.StartFunTime = datetime.datetime.now()
+		end_time = FunState.StartFunTime + datetime.timedelta(seconds=FunState.RestTime)
+		user.send_msg(
+			f"你的娱乐时间还剩余{math.floor(FunState.RestTime / 60)}分钟，截止时间为{end_time.hour}:{end_time.minute}，请注意不要娱乐过度！")
+
+		def stop_fun_tip():
+			FunState.RestTime = 0
+			user.send_msg("你已经没有娱乐时间了，请开始你的学习为你的娱乐时间充值！")
+
+		self.WarningT = Timer(FunState.RestTime, stop_fun_tip)
+		self.WarningT.start()
+
+	def on_exit(self, user):
+		super().on_exit(user)
+
+		self.WarningT.cancel()
+
+		FunState.RestTime -= (datetime.datetime.now() - FunState.StartFunTime).seconds
+		if FunState.RestTime < 0:
+			user.send_msg(f"你已经过度娱乐，超出了{math.ceil(abs(FunState.RestTime) / 60)}分，请开始你的学习！")
+		else:
+			user.send_msg(f"你的娱乐时间剩余{math.floor(FunState.RestTime / 60)}分")
+
+	@classmethod
+	def can_switch_to(cls, config, sender, msg):
+		if sender.NickName == "cmdr":
+			if FunState.StartFunTime is None or datetime.datetime.now().day != FunState.StartFunTime.day:
+				FunState.RestTime = config["fun"] * 3600
+				return True
+			elif FunState.RestTime > 0:
+				return True
+			else:
+				msg.user.send_msg(f"你已经过度娱乐，超出了{math.ceil(abs(FunState.RestTime) / 60)}分，请开始你的学习！")
+
+		return False
+
+	def text_reply(self, sender, msg):
+		super().text_reply(sender, msg)
+
+		if sender.NickName == "cmdr" and msg.text == "停止娱乐":
+			self.owner.try_to(IdleState, sender, msg)
+		elif sender.NickName == "cmdr" and msg.text.startswith("开始"):
+			self.owner.try_to(StudyState, sender, msg)
+
+
+class StudyState(StateBase):
+	StudyType = None
+	StartTime = None
+	Win = False
+
+	@classmethod
+	def can_switch_to(cls, config, sender, msg):
+		if sender.NickName == "cmdr":
+			action = msg.text[len("开始"):]
+			if action in config["study"]:
+				StudyState.StudyType = action
+				return True
+
+		return False
+
+	def on_enter(self, user):
+		super().on_enter(user)
+
+		StudyState.StartTime = datetime.datetime.now()
+
+		user.send_msg("收到，开始统计学习时间")
+
+	def on_exit(self, user):
+		super().on_exit(user)
+
+		if type(self.config["study"][self.StudyType]) is list:
+			if self.Win:
+				rate = self.config["study"][self.StudyType][0]
+			else:
+				rate = self.config["study"][self.StudyType][1]
+		else:
+			rate = self.config["study"][self.StudyType]
+
+		study_time = (datetime.datetime.now() - StudyState.StartTime).seconds
+		charge_time = study_time * rate
+		FunState.RestTime += charge_time
+		user.send_msg(
+			f"本次学习时长为{study_time}秒，倍率为{rate}，充值时间为{charge_time}秒，剩余娱乐时间为{math.floor(FunState.RestTime / 60)}分钟")
+
+	def text_reply(self, sender, msg):
+		super().text_reply(sender, msg)
+
+		if self.StudyType is not None and msg.text == self.StudyType + "结束":
+			if self.StudyType == "围棋":
+				msg.user.send_msg("请发送围棋赢了或者围棋输了")
+			else:
+				return True
+
+		elif self.StudyType == "围棋" and ("赢了" in msg.text or "输了" in msg.text):
+			if "赢了" in msg.text:
+				self.Win = True
+			else:
+				self.Win = False
+			return True
+
+		return False
--- main.py
+++ main.py
@@ -4,100 +4,22 @@ from ItChat import itchat
 import datetime
 from ItChat.itchat.content import *
 from threading import Timer
+from State import StateOwner
 
-start_fun_time: datetime.datetime | None = None
-start_study_time: datetime.datetime | None = None
-study_action = None
-fun_rest_time = 60 * 1
-warning_t = None
-
+owner = None
 config = None
 
 
 @itchat.msg_register(TEXT, isGroupChat=True)
 def text_reply(msg):
-	global fun_rest_time, warning_t, start_fun_time, study_action, start_study_time
-	print(msg)
-	if msg.user.NickName == "个个都是靓仔靓女":
-		sender = msg.user.search_member(userName=msg.ActualUserName)
-		print(msg.text, sender)
-
-		if sender.NickName == "cmdr":
-			if msg.text == "开始娱乐":
-
-				if start_fun_time == None or datetime.datetime.now().day != start_fun_time.day:
-					fun_rest_time = 3600 * config["fun"]
-
-				if fun_rest_time > 0:
-					start_fun_time = datetime.datetime.now()
-					end_time = start_fun_time + datetime.timedelta(seconds=fun_rest_time)
-					msg.user.send_msg(f"你的娱乐时间还剩余{math.floor(fun_rest_time / 60)}分钟，截止时间为{end_time.hour}:{end_time.minute}，请注意不要娱乐过度！")
-
-					def stop_fun_tip():
-						global fun_rest_time
-						fun_rest_time = 0
-						msg.user.send_msg("你已经没有娱乐时间了，请开始你的学习为你的娱乐时间充值！")
-
-					warning_t = Timer(fun_rest_time, stop_fun_tip)
-					warning_t.start()
-				else:
-					msg.user.send_msg(f"你已经没有娱乐时间了，请开始你的学习为你的娱乐时间充值！")
-			elif msg.text == "娱乐停止":
-				warning_t.cancel()
-				if fun_rest_time > 0:
-					fun_rest_time -= (datetime.datetime.now() - start_fun_time).seconds
-
-				if fun_rest_time < 0:
-					msg.user.send_msg(f"你已经过度娱乐，超出了{math.floor(fun_rest_time)}秒，请开始你的学习！")
-				else:
-					msg.user.send_msg(f"你的娱乐时间剩余{math.floor(fun_rest_time / 60)}分")
-			elif msg.text.startswith("开始"):
-				action = msg.text[len("开始"):]
-				if action in config["study"]:
-					study_action = action
-					start_study_time = datetime.datetime.now()
-					msg.user.send_msg("收到，开始统计学习时间")
-
-			elif study_action is not None and msg.text == study_action + "结束":
-				if study_action == "围棋":
-					msg.user.send_msg("围棋赢了还是输了？")
-				else:
-					charge(msg)
-
-			elif study_action == "围棋" and ("赢了" in msg.text or "输了" in msg.text):
-				if "赢了" in msg.text:
-					win = True
-				else:
-					win = False
-
-				charge(msg, win)
-
-
-def charge(msg, win=False):
-	global fun_rest_time, study_action, start_study_time
-	study_type = study_action
-	study_time = (datetime.datetime.now() - start_study_time).seconds
-	if type(config["study"][study_type]) is list:
-		if win:
-			rate = config["study"][study_type][0]
-		else:
-			rate = config["study"][study_type][1]
-	else:
-		rate = config["study"][study_type]
-
-	charge_time = study_time * rate
-	fun_rest_time += charge_time
-	msg.user.send_msg(
-		f"本次学习时长为{study_time}秒，倍率为{rate}，充值时间为{charge_time}秒，剩余娱乐时间为{math.floor(fun_rest_time / 60)}分钟")
-
-	study_action = None
-	start_study_time = 0
+	owner.text_reply(msg)
 
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
 	with open("config.json", "r", encoding="utf-8") as f:
 		config = json.load(f)
+		owner = StateOwner(config)
 
 	itchat.auto_login(enableCmdQR=2)
 	itchat.run()
